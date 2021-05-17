687bee9e925ad902666a2abbbba18b7c2313f437
Gabrirf <gabrodflo@alum.us.es>
2021-05-17 20:08:24 +0200

    Add react router dom on header

--- src/Header.js
+++ src/Header.js
@@ -2,37 +2,33 @@ import React from 'react';
 import {
   Navbar,
   Nav,
+  NavItem,
   NavDropdown,
   Form,
   FormControl,
   Button
 } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
-    <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#home">My Pokemon List</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
+    <Navbar>
+      <Navbar.Brand as={Link} to="/">
+        My Pokemon List
+      </Navbar.Brand>
+      <Navbar.Collapse>
         <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-              Separated link
-            </NavDropdown.Item>
-          </NavDropdown>
+          <NavItem eventkey={1} href="/">
+            <Nav.Link as={Link} to="/">
+              Home
+            </Nav.Link>
+          </NavItem>
+          <NavItem eventkey={2} href="/search">
+            <Nav.Link as={Link} to="/search">
+              Search
+            </Nav.Link>
+          </NavItem>
         </Nav>
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-success">Search</Button>
-        </Form>
       </Navbar.Collapse>
     </Navbar>
   );
